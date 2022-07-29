4c9c85bc8b2ff01b0632c61c9d61823bd0e8d5b3
xjd <lynndon@gmail.com>
2022-07-29 11:15:44 +0800

    Batch converting

--- py/process_profile.py
+++ py/process_profile.py
@@ -1,16 +1,48 @@
 import re
 import sys
 import os
+from subprocess import Popen, PIPE, STDOUT
+
 #
-# use 
+# use
 # cargo install --git https://github.com/gimli-rs/addr2line.git --example addr2line
 # to install addr2line
 #
-PROGRAM="/home/xjd/projects/rvv-prototype/bn128-example/target/riscv64imac-unknown-none-elf/release/alt-bn128-example-rvv-asm-bench"
+
+ADDR2LINE = "/home/xjd/.cargo/bin/addr2line"
+PROGRAM = "/home/xjd/projects/rvv-prototype/bn128-example/target/riscv64imac-unknown-none-elf/release/alt-bn128-example-rvv-asm-bench"
 
 # @[197698]: 581
 RE = re.compile(r"@\[(\d+)\]: (\d+)")
 RE2 = re.compile(r"(.+) at (.+)")
+
+
+def parse_addr(addrs):
+    args = [f'{ADDR2LINE}', '-C', '-p', '-f', '-e', f"{PROGRAM}"]
+    p = Popen(args, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    stdout = p.communicate(input="\n".join(addrs).encode())[0]
+    ret = []
+    count = 0
+    for line in stdout.decode().split("\n"):
+        m2 = RE2.match(line)
+        count += 1
+        if m2:
+            name = m2.group(1)
+            ret += [name]
+    assert(len(ret) == len(addrs))
+    return ret
+
+
+def merge(stats):
+    d = {}
+    for i in stats:
+        if i[0] in d:
+            d[i[0]] += i[1]
+        else:
+            d[i[0]] = i[1]
+
+    return [(i, d[i]) for i in d]
+
 
 stats = {}
 
@@ -24,26 +56,19 @@ for line in sys.stdin:
 # skip zero
 del stats[0]
 
-stats2 = {}
+stats2 = []
 total = 0
 for addr in stats:
     count = stats[addr]
     total += count
-
-    cmd = f"addr2line -C -p -f -e {PROGRAM} {addr:#08x}"
-    for line in os.popen(cmd):
-        m2 = RE2.match(line)
-        if m2:
-            name = m2.group(1)
-            if name in stats2:
-                stats2[name] += count
-            else:
-                stats2[name] = count
+    stats2 += [(f"{addr:#08x}", count)]
 
 print(f"total sampling count = {total}")
-stats3 = []
-for key in stats2:
-    stats3 += [(key, stats2[key])]
+names = parse_addr([i[0] for i in stats2])
+
+stats3 = [(i, j) for i, j in zip(names, [i[1] for i in stats2])]
+
+stats3 = merge(stats3)
 
 stats3.sort(key=lambda x: x[1])
 
